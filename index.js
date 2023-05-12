f21ed392e5699aa57f2e5344958d7ffd4a3f35ea
Danylo Diachenko <danildiachenko23@gmail.com>
2023-05-12 17:43:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const mongoose = require('mongoose');
 const coffeeRoutes = require('./routes/coffee-routes');
 require('dotenv').config();
 
-const PORT = 3000;
-
 const app = express();
 app.use(express.json());
 app.use(coffeeRoutes);
 
-app.listen(PORT, () => {
-	console.log(`Server started on port ${PORT}`);
+app.listen(5000, () => {
+	console.log(`Server started on port ${5000}`);
 });
 
 mongoose

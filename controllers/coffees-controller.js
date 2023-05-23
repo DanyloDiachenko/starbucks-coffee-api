fbba1afaeb37327db7803d4fa6976e05dbb0179c
Danylo Diachenko <danildiachenko23@gmail.com>
2023-05-23 19:22:19 +0300

    /coffee/:id

--- controllers/coffees-controller.js
+++ controllers/coffees-controller.js
@@ -2,7 +2,7 @@ const { Coffee } = require('../models/coffee');
 
 const printError = (res, error) => {
 	res
-		.statusCode(500)
+		.status(500)
 		.json({ success: false, error: error })
 };
 
@@ -17,6 +17,19 @@ const getCoffees = (req, res) => {
 		.catch((err) => printError(res, err));
 };
 
+const getCoffee = (req, res) => {
+	const coffeeId = req.params.id;
+	Coffee
+		.findById(coffeeId)
+		.then(coffee => {
+			res
+				.status(200)
+				.json({ success: true, data: coffee })
+		})
+		.catch((err) => printError(res, err))
+};
+
 module.exports = {
 	getCoffees,
+	getCoffee
 };
--- routes/coffee-routes.js
+++ routes/coffee-routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getCoffees } = require('../controllers/coffees-controller');
+const { getCoffees, getCoffee } = require('../controllers/coffees-controller');
 
 const router = express.Router();
 
 router.get('/coffees', getCoffees);
+router.get('/coffees/:id', getCoffee)
 
 module.exports = router;
